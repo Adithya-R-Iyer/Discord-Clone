a077aa9341b98b1487a5ea3dd3b290ac6e8044d9
Adithya <adikrishna1972@gmail.com>
2023-09-01 22:21:44 +0530

    Authentication setup Complete... with Clerk

--- discord-clone/app/(main)/(routes)/page.tsx
+++ discord-clone/app/(main)/(routes)/page.tsx
@@ -1,9 +1,10 @@
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='flex flex-col'>
-      This is a protected route
+    <div>
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
