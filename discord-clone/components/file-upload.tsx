a175e15af23386f0685c03efdd537b472ec7f84b
Adithya <adikrishna1972@gmail.com>
2023-09-03 01:27:36 +0530

    Preview of the Image Uploaded on FrontEnd ...Completegit log -1 100%

--- discord-clone/components/file-upload.tsx
+++ discord-clone/components/file-upload.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { X } from "lucide-react";
+import Image from "next/image";
+
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
 
@@ -10,9 +13,24 @@ interface FileUploadProps {
 }
 
 const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="upload" className="rounded-full"></Image>
+        <button
+            onClick={() => onChange("")}
+            className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+        >
+            <X className="h-4 w-4"/>
+        </button>
+      </div>
+    );
+  }
+
   return (
     <UploadDropzone
-      endpoint= {endpoint}
+      endpoint={endpoint}
       onClientUploadComplete={(res) => {
         // Do something with the response
         onChange(res?.[0].url);
