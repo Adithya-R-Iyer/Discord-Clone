6697a5073aeb7698dec5515959427f07638ba2e4
Adithya <adikrishna1972@gmail.com>
2023-09-04 00:08:00 +0530

    NavigationItem Design Complete... or Server Icons design and render complete ...setup with database

--- discord-clone/components/navigation/navigation-sidebar.tsx
+++ discord-clone/components/navigation/navigation-sidebar.tsx
@@ -1,9 +1,13 @@
 import { redirect } from "next/navigation";
 
-import NavigationAction from "@/components/navigation/navigation-action";
-
-import currentProfile from "@/lib/current-profile";
 import { db } from "@/lib/db";
+import currentProfile from "@/lib/current-profile";
+
+import NavigationAction from "@/components/navigation/navigation-action";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import NavigationItem from "@/components/navigation/navigation-item";
+
 
 
 const NavigationSidebar = async () => {
@@ -29,6 +33,19 @@ const NavigationSidebar = async () => {
             className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3"
         >
             <NavigationAction/>
+            <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+            <ScrollArea className="flex-1 w-full">
+                {servers.map((server)=> (
+                    <div key={server.id} className="mb-4">
+                        <NavigationItem
+                            id={server.id}
+                            imageUrl={server.imageUrl}
+                            name={server.name}
+                        />
+                    </div>
+                ))}
+            </ScrollArea>
+            
         </div>
      );
 }
