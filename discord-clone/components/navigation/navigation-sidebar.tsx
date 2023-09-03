5d29fce8b92b46c59630aadac33eee3d18745a5d
Adithya <adikrishna1972@gmail.com>
2023-09-04 00:16:08 +0530

    SideBar Design Complete - 100%

--- discord-clone/components/navigation/navigation-sidebar.tsx
+++ discord-clone/components/navigation/navigation-sidebar.tsx
@@ -7,6 +7,8 @@ import NavigationAction from "@/components/navigation/navigation-action";
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import NavigationItem from "@/components/navigation/navigation-item";
+import { ModeToggle } from "../mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 
 
@@ -45,7 +47,17 @@ const NavigationSidebar = async () => {
                     </div>
                 ))}
             </ScrollArea>
-            
+            <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+                <ModeToggle/>
+                <UserButton
+                    afterSignOutUrl="/"
+                    appearance={{
+                        elements: {
+                            avatarBox: "h-[48px] w-[48px]"
+                        }
+                    }}
+                />
+            </div>
         </div>
      );
 }
