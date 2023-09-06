39b8afaa7bafdec7c03ba5bfad977106da616d82
Adithya <adikrishna1972@gmail.com>
2023-09-07 00:25:58 +0530

    Search Bar Design and Implementation complete 100% - Chat Routes to be defined

--- discord-clone/components/server/server-search.tsx
+++ discord-clone/components/server/server-search.tsx
@@ -1,6 +1,16 @@
 "use client"
 
-import { ChevronUpSquare, Search } from "lucide-react";
+import {
+    CommandDialog,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandList,
+} from "@/components/ui/command";
+import { CommandItem } from "cmdk";
+import { Search } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface ServerSearchProps {
     data: {
@@ -17,9 +27,39 @@ interface ServerSearchProps {
 const ServerSearch = ({
     data
 } : ServerSearchProps ) => {
+
+    const [open, setOpen] = useState(false)
+    const router = useRouter();
+    const params = useParams();
+
+    useEffect(() => {
+        const down = (e: KeyboardEvent) => {
+          if (e.key === "f" && (e.metaKey || e.ctrlKey)) {
+            e.preventDefault()
+            setOpen((open) => !open)
+          }
+        }
+        
+        document.addEventListener("keydown", down)
+        return () => document.removeEventListener("keydown", down)
+      }, []);
+
+      const onClick = ({ id, type } : { id: string, type: "channel" | "member" }) => {
+        setOpen(false);
+
+        if(type=="member") {
+            return router.push(`/servers/${params?.serverId}/conversations/${id}`);
+        }
+
+        if(type=="channel") {
+            return router.push(`/servers/${params?.serverId}/channels/${id}`)
+        }
+      }
+
     return ( 
         <div>
             <button
+                onClick={()=> setOpen(true)}
                 className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
             >
                 <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400"/>
@@ -32,6 +72,32 @@ const ServerSearch = ({
                     <span className="text-xs">CTRL</span>F
                 </kbd>
             </button>
+            <CommandDialog open={open} onOpenChange={setOpen}>
+                <CommandInput placeholder="Search all channels and members" />
+                <CommandList>
+                    <CommandEmpty>No results found.</CommandEmpty>
+                    {data.map(({ label, type, data })=> {
+                        if(!data?.length) return null;
+
+                        return(
+                            <CommandGroup key={label} heading={label}>
+                                {data?.map(({ icon, name ,id })=> {
+                                    return (
+                                        <CommandItem
+                                            key={id}
+                                            className="flex items-center text-sm cursor-pointer"
+                                            onSelect={()=> onClick({id, type})}    
+                                        >
+                                            {icon}
+                                            <span>{name}</span>
+                                        </CommandItem>
+                                    );
+                                })}
+                            </CommandGroup>
+                        );
+                    })}
+                </CommandList>
+            </CommandDialog>
         </div>
      );
 }
