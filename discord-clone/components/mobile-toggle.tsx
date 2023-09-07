3bccd8fedcdd8ea0f535d2259166679376b9f5f3
Adithya <adikrishna1972@gmail.com>
2023-09-08 01:59:04 +0530

    Updated Prisma Schema to support messages and conversations

--- discord-clone/components/mobile-toggle.tsx
+++ discord-clone/components/mobile-toggle.tsx
@@ -1,15 +1,32 @@
+// "use client"
+
 import { Menu } from "lucide-react";
 
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetFooter, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import NavigationSidebar from "./navigation/navigation-sidebar";
 import ServerSidebar from "./server/server-sidebar";
+// import { useEffect } from "react";
 
 interface MobileToggleProps {
     serverId: string;
 }
 
 const MobileToggle = ( {serverId} : MobileToggleProps) => {
+
+  // useEffect(()=> {
+    
+  //   const closeButton = document.getElementById('close');
+  //   const handleScreenChange = ()=> {
+  //     if(window.innerWidth>=768) {
+  //       closeButton?.click();
+  //     }
+  //   }
+  //   window.addEventListener("resize", handleScreenChange);
+
+  //   return ()=> window.removeEventListener("resize", handleScreenChange);
+  // },[])
+
   return (
     <div>
       <Sheet>
@@ -18,12 +35,17 @@ const MobileToggle = ( {serverId} : MobileToggleProps) => {
             <Menu />
           </Button>
         </SheetTrigger>
-        <SheetContent side="left" className="p-0 flex gap-0">
+        <SheetContent side="left" className="p-0 flex gap-0 md:hidden ">
             <div className="w-[72px]">
                 <NavigationSidebar/>
             </div>
             <ServerSidebar serverId={serverId}/>
         </SheetContent>
+        {/* <SheetFooter>
+          <SheetClose asChild>
+            <Button id="close" className="hidden">Close</Button>
+          </SheetClose>
+        </SheetFooter> */}
       </Sheet>
     </div>
   );
