8fcc17cf4f09ca85ce492ae714f3a5b7261f13f1
Adithya <adikrishna1972@gmail.com>
2023-09-10 21:45:02 +0530

    Status Chat Input Design Complete.. Attachments Dialog yet to be designed

--- discord-clone/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ discord-clone/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
 import currentProfile from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -41,6 +42,18 @@ const ChannelIdPage = async ({
     return ( 
         <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
             <ChatHeader serverId={channel.serverId} type="channel" name={channel.name} />
+            <div className="flex-1">
+                Future Messages to be implemented
+            </div>
+            <ChatInput
+                name={channel.name}
+                type="channel"
+                apiUrl="/api/socket/messages"
+                query={{
+                    channelId: channel.id,
+                    serverId: channel.serverId,
+                }}
+            />
         </div>
      );
 }
